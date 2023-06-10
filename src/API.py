31d58875337f9bed300d52e2ab082e3242084f18
Arthur Henrique Silva de Lima Arthur <115894179+ArthurHenriqueSilva@users.noreply.github.com>
2023-06-10 17:14:45 -0300

    Criação de rotas da API - Arthur

--- src/API.py
+++ src/API.py
@@ -90,3 +90,62 @@ class UF(db.Model):
     def __init__(self, nome, nome_ex):
         self.nome = nome
         self.nome_extenso = nome_ex
+
+
+# ------------- ROTAS DA API -------------
+@app.route('/residentes', methods=['POST'])
+def cadastrar_residente(uf, pais, qtd):
+    new_residente = Residente(uf, pais, qtd)
+
+@app.route('/provisorios', methods=['POST'])
+def cadastrar_provisorio(uf, pais, qtd):
+    new_provisorio = Provisorio(uf, pais, qtd)
+
+@app.route('/temporarios', methods=['POST'])
+def cadastrar_temporario(uf, pais, qtd):
+    new_temporario = Temporario(uf, pais, qtd)
+
+
+
+@app.route('/paises', methods=["POST"])
+def cadastrar_pais(nome_pais):
+    new_pais = Pais(nome_pais)
+
+@app.route('/ufs', methods=['POST'])
+def cadastrar_uf(sigla):
+    # nome_extenso_dict será um dicionario com key=sigla e value=nome completo da UF
+    new_uf = UF(sigla, nome_extenso_dict[sigla])
+
+@app.route('/registros', methods=['POST'])
+def cadastrar_registro():
+    new_registro = Registro(
+        request.json['uf'],
+        request.json['pais'],
+        request.json['classificacao'],
+        request.json['qtd']
+    )
+
+    classificacao = new_registro.classificacao.lower() 
+
+    cadastrar_pais(new_registro.pais)
+    cadastrar_uf(new_registro.uf)
+    
+    if(classificacao == 'residente'):
+        cadastrar_residente(new_registro.uf, new_registro.pais, new_registro.qtd)
+    elif(classificacao == 'provisório'):
+        cadastrar_provisorio(new_registro.uf, new_registro.pais, new_registro.qtd)
+    else:
+        cadastrar_temporario(new_registro.uf, new_registro.pais, new_registro.qtd)
+
+
+    db.session.add(new_registro)
+    db.session.commit()
+
+    return jsonify({
+        'id':new_registro.id,
+        'uf':new_registro.uf,
+        'pais':new_registro.pais,
+        'classificacao':new_registro.classificacao,
+        'qtd':new_registro.qtd
+    })
+# ------------- ROTAS DA API -------------
