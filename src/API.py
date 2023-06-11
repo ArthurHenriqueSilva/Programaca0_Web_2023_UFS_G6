fd7cdd5729fd321a419425bdabccd35632b6a136
Arthur Henrique Silva de Lima Arthur <115894179+ArthurHenriqueSilva@users.noreply.github.com>
2023-06-11 14:18:44 -0300

    Add files via upload

--- src/API.py
+++ src/API.py
@@ -1,12 +1,37 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Date, ForeignKey, Integer, String, Column, Float, DateTime, Boolean
-from sqlalchemy.orm import Mapped, Session
-from sqlalchemy_utils import JSONType
 
-import jwt
-import os
-from datetime import datetime as dt, timedelta
+# Dicionário com correspondência de Estados ->  Sigla:Nome_extenso 
+estados = {
+    'AC': 'Acre',
+    'AL': 'Alagoas',
+    'AP': 'Amapá',
+    'AM': 'Amazonas',
+    'BA': 'Bahia',
+    'CE': 'Ceará',
+    'DF': 'Distrito Federal',
+    'ES': 'Espírito Santo',
+    'GO': 'Goiás',
+    'MA': 'Maranhão',
+    'MT': 'Mato Grosso',
+    'MS': 'Mato Grosso do Sul',
+    'MG': 'Minas Gerais',
+    'PA': 'Pará',
+    'PB': 'Paraíba',
+    'PR': 'Paraná',
+    'PE': 'Pernambuco',
+    'PI': 'Piauí',
+    'RJ': 'Rio de Janeiro',
+    'RN': 'Rio Grande do Norte',
+    'RS': 'Rio Grande do Sul',
+    'RO': 'Rondônia',
+    'RR': 'Roraima',
+    'SC': 'Santa Catarina',
+    'SP': 'São Paulo',
+    'SE': 'Sergipe',
+    'TO': 'Tocantins'
+}
+
 
 app = Flask(__name__)  # create Flask app
 
@@ -92,32 +117,51 @@ class UF(db.Model):
         self.nome_extenso = nome_ex
 
 
-# ------------- ROTAS DA API -------------
-@app.route('/residentes', methods=['POST'])
+
+# ---------- Funções Aux DA API -------------
+# Cadastro de Imigrante Residente
 def cadastrar_residente(uf, pais, qtd):
     new_residente = Residente(uf, pais, qtd)
+    db.session.add(new_residente)
+    db.session.commit()
 
-@app.route('/provisorios', methods=['POST'])
+# Cadastro de Imigrante Provisório
 def cadastrar_provisorio(uf, pais, qtd):
     new_provisorio = Provisorio(uf, pais, qtd)
+    db.session.add(new_provisorio)
+    db.session.commit()
 
-@app.route('/temporarios', methods=['POST'])
+# Cadastro de Imigrante Temporário
 def cadastrar_temporario(uf, pais, qtd):
     new_temporario = Temporario(uf, pais, qtd)
+    db.session.add(new_temporario)
+    db.session.commit()
 
-
-
-@app.route('/paises', methods=["POST"])
+# Cadastro de País
 def cadastrar_pais(nome_pais):
     new_pais = Pais(nome_pais)
+    db.session.add(new_pais)
+    db.session.commit()
 
-@app.route('/ufs', methods=['POST'])
+# Cadastro de UF
 def cadastrar_uf(sigla):
-    # nome_extenso_dict será um dicionario com key=sigla e value=nome completo da UF
-    new_uf = UF(sigla, nome_extenso_dict[sigla])
+    new_uf = UF(sigla, estados[sigla])
+    db.session.add(new_uf)
+    db.session.commit()
+# ---------- Funções Aux DA API -------------
 
+
+
+
+# ------------- ROTAS DA API -------------
+# Executa o cadastro linha a linha dos arquivos csv. Aciona a execução dos cadastros secundários
 @app.route('/registros', methods=['POST'])
 def cadastrar_registro():
+    # Verifica se a solicitação possui todos os campos necessários
+    if not request.json or 'uf' not in request.json or 'pais' not in request.json or 'classificacao' not in request.json or 'qtd' not in request.json:
+        return jsonify({'error': 'Solicitação inválida. Certifique-se de fornecer todos os campos necessários.'}), 400
+
+    # Cria um novo registro
     new_registro = Registro(
         request.json['uf'],
         request.json['pais'],
@@ -125,27 +169,32 @@ def cadastrar_registro():
         request.json['qtd']
     )
 
-    classificacao = new_registro.classificacao.lower() 
+    # Converte a classificação para minúsculas
+    classificacao = new_registro.classificacao.lower()
 
+    # Cadastra o país e o estado
     cadastrar_pais(new_registro.pais)
     cadastrar_uf(new_registro.uf)
-    
-    if(classificacao == 'residente'):
+
+    # Executa a ação apropriada com base na classificação
+    if classificacao == 'residente':
         cadastrar_residente(new_registro.uf, new_registro.pais, new_registro.qtd)
-    elif(classificacao == 'provisório'):
+    elif classificacao == 'provisório':
         cadastrar_provisorio(new_registro.uf, new_registro.pais, new_registro.qtd)
     else:
         cadastrar_temporario(new_registro.uf, new_registro.pais, new_registro.qtd)
 
-
+    # Adiciona o novo registro ao banco de dados
     db.session.add(new_registro)
     db.session.commit()
 
+    # Retorna os dados do registro inserido e o código de status 200
     return jsonify({
-        'id':new_registro.id,
-        'uf':new_registro.uf,
-        'pais':new_registro.pais,
-        'classificacao':new_registro.classificacao,
-        'qtd':new_registro.qtd
-    })
+        'id': new_registro.id,
+        'uf': new_registro.uf,
+        'pais': new_registro.pais,
+        'classificacao': new_registro.classificacao,
+        'qtd': new_registro.qtd
+    }), 200
+
 # ------------- ROTAS DA API -------------
