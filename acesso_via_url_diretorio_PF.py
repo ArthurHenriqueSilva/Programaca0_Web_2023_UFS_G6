f0386bb3bc0d8b78f27275bd50ac4d6dad3783f5
Arthur Henrique Silva de Lima Arthur <115894179+ArthurHenriqueSilva@users.noreply.github.com>
2023-06-12 01:11:49 -0300

    Teste de funcionalidade

--- acesso_via_url_diretorio_PF.py
+++ acesso_via_url_diretorio_PF.py
@@ -15,17 +15,27 @@ for link in soup.find_all('a'):
     if re.match(r'SISMIGRA_REGISTROS_ATIVOS_2022_\d{2}.csv', file_url):
         file_links.append(directory_url + file_url)
 
+
+
+'''
+print(file_links)
+lista esperada:  ['https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_01.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_02.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_03.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_04.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_05.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_06.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_07.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_08.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_09.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_10.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_11.csv', 
+'https://servicos.dpf.gov.br/dadosabertos/SISMIGRA/SISMIGRA_REGISTROS_ATIVOS_2022_12.csv']
+'''
 for file_url in file_links:
     response = requests.get(file_url)
     csv_content = response.content
-
-    dados = csv_content.strip().split(';')
-    if dados[0] != 'UF':
-            dados_json = {
-                'uf':dados[0],
-                'pais':dados[1],
-                'classificacao':dados[2],
-                'qtd':dados[3]
-            }
-
-    print(dados)
+    csv_text = csv_content.decode('utf-8').replace('\r', '').strip()
+    rows = csv_text.split('\n')
+    data = [row.split(';') for row in rows]
+    print(data)
