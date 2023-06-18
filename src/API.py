9ea4f98f4b1cebf01320f5cd279b6427ee7432e2
Raulshadow <50181593+Raulshadow@users.noreply.github.com>
2023-06-18 12:15:18 -0300

    Adição de 3 consultas
    Consultas baseadas nas perguntas 2,3,4  solicitadas pelo professor.

--- src/API.py
+++ src/API.py
@@ -180,6 +180,32 @@ def cadastrar_uf(sigla):
     db.session.add(new_uf)
     db.session.commit()
 
+# Consulta de qual país com mais imigração Q2
+def consulta_pais_imigracao():
+    pais = db.session.query(Registro.pais, db.func.sum(Registro.qtd).label('Total'))\
+                    .group_by(Registro.pais)\
+                    .order_by(db.desc('Total'))\
+                    .first()
+
+    return str(pais.pais)
+
+# Consulta de qual é o tipo principal de imigrante que recebemos no brasil Q3
+def consulta_tipo_imigrante():
+    tipo = db.session.query(Registro.classificacao, db.func.sum(Registro.qtd).label('Total'))\
+                    .group_by(Registro.classificacao)\
+                    .order_by(db.desc('Total'))\
+                    .first()
+
+    return str(tipo.classificacao)
+
+# Consulta de qual é o período do ano que recebmos mais imigrantes Q4
+def consulta_periodo_popular():
+    periodo = db.session.query(Registro.mes, db.func.sum(Registro.qtd).label('Total'))\
+                    .group_by(Registro.mes)\
+                    .order_by(db.desc('Total'))\
+                    .first()
+
+    return str(periodo.mes)
 # ---------- Funções Aux DA API -------------
 
 
