799c65f6f8bf36a5419e39d3f4626e0c03d8bcba
Raulshadow <50181593+Raulshadow@users.noreply.github.com>
2023-06-27 16:02:23 -0300

    Alteração da API_nova para API, mudança de lugar e arquivos e atualização do control
    mudança de pasta
    alteração da API
    inserção do funcionamento no control.py

--- src/API.py
+++ src/API.py
@@ -1,136 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
-# Dicionário com correspondência de Estados ->  Sigla:Nome_extenso 
-estados = {
-    'AC': 'Acre',
-    'AL': 'Alagoas',
-    'AP': 'Amapá',
-    'AM': 'Amazonas',
-    'BA': 'Bahia',
-    'CE': 'Ceará',
-    'DF': 'Distrito Federal',
-    'ES': 'Espírito Santo',
-    'GO': 'Goiás',
-    'MA': 'Maranhão',
-    'MT': 'Mato Grosso',
-    'MS': 'Mato Grosso do Sul',
-    'MG': 'Minas Gerais',
-    'PA': 'Pará',
-    'PB': 'Paraíba',
-    'PR': 'Paraná',
-    'PE': 'Pernambuco',
-    'PI': 'Piauí',
-    'RJ': 'Rio de Janeiro',
-    'RN': 'Rio Grande do Norte',
-    'RS': 'Rio Grande do Sul',
-    'RO': 'Rondônia',
-    'RR': 'Roraima',
-    'SC': 'Santa Catarina',
-    'SP': 'São Paulo',
-    'SE': 'Sergipe',
-    'TO': 'Tocantins',
-    '--':'Indefinido'
-}
-
+from aux_data import estados, meses
 
 app = Flask(__name__)  # create Flask app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:chaveacesso@db-instance-prog-web.cuokvhdjyvdp.us-east-1.rds.amazonaws.com/Database_SISMIGRA'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-
-class Registro(db.Model):
-    __tablename__ = "Registro"
-
-    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-    uf = db.Column(db.String(2), db.ForeignKey('UF.nome'))
-    pais = db.Column(db.String(50), db.ForeignKey('Pais.nome'))
-    Classificacao = db.Column(db.String(15))
-    qtd = db.Column(db.Integer())
-    mes = db.Column(db.Integer())
-
-    def __init__(self, uf, pais, classificacao, qtd, mes):
-        self.uf = uf
-        self.pais = pais
-        self.classificacao = classificacao
-        self.qtd = qtd
-        self.mes = mes
-
-
-class Residente(db.Model):
-    __tablename__ = 'Residente'
-
-    id = db.Column(db.Integer(), db.ForeignKey('Registro.id'), primary_key=True,)
-    uf = db.Column(db.String(2), db.ForeignKey('UF.nome'))
-    pais = db.Column(db.String(50), db.ForeignKey('Pais.nome'))
-    qtd = db.Column(db.Integer())
-
-    def __init__(self, uf, pais, qtd):
-        self.uf = uf
-        self.pais = pais
-        self.qtd = qtd
-
-class Provisorio(db.Model):
-    __tablename__ = 'Provisorio'
-    
-    id = db.Column(db.Integer(), db.ForeignKey('Registro.id'), primary_key=True)
-    uf = db.Column(db.String(2), db.ForeignKey('UF.nome'))
-    pais = db.Column(db.String(50), db.ForeignKey('Pais.nome'))
-    qtd = db.Column(db.Integer())
-
-    def __init__(self, uf, pais, qtd):
-        self.uf = uf
-        self.pais = pais
-        self.qtd = qtd
-
-class Temporario(db.Model):
-    __tablename__ = 'Temporario'
-    
-    id = db.Column(db.Integer(), db.ForeignKey('Registro.id'), primary_key=True)
-    uf = db.Column(db.String(2), db.ForeignKey('UF.nome'))
-    pais = db.Column(db.String(50), db.ForeignKey('Pais.nome'))
-    qtd = db.Column(db.Integer())
-
-    def __init__(self, uf, pais, qtd):
-        self.uf = uf
-        self.pais = pais
-        self.qtd = qtd
-
-class Fronteirico(db.Model):
-    __tablename__ = 'Fronteirico'
-    
-    id = db.Column(db.Integer(), db.ForeignKey('Registro.id'), primary_key=True)
-    uf = db.Column(db.String(2), db.ForeignKey('UF.nome'))
-    pais = db.Column(db.String(50), db.ForeignKey('Pais.nome'))
-    qtd = db.Column(db.Integer())
-
-    def __init__(self, uf, pais, qtd):
-        self.uf = uf
-        self.pais = pais
-        self.qtd = qtd
-
-
-class Pais(db.Model):
-    __tablename__ = 'Pais'
-
-    nome = db.Column(db.String(50), primary_key=True)
-
-    def __init__(self, nome):
-        self.nome = nome
-
-
-class UF(db.Model):
-    __tablename__ = 'UF'
-
-    nome = db.Column(db.String(2), primary_key=True)
-    nome_extenso = db.Column(db.String(15))
-
-    def __init__(self, nome, nome_ex):
-        self.nome = nome
-        self.nome_extenso = nome_ex
-
-
+from models import Registro, Residente, Provisorio, Temporario, Fronteirico, Pais, UF
 
 # ---------- Funções Aux DA API -------------
 # Cadastro de Imigrante Residente
@@ -180,59 +56,225 @@ def cadastrar_uf(sigla):
     db.session.add(new_uf)
     db.session.commit()
 
-# ---------- Funções Aux DA API -------------
+# 1: Qual a distribuição de imigrantes do país X?
+
+def consulta_distribuicao_imigrantes_pais(pais_filtro):
+    pais_filtro = pais_filtro.upper()
+    with app.app_context():
+        distribuicao = db.session.query(Registro.classificacao, db.func.sum(Registro.qtd).label('Total')) \
+            .filter(Registro.pais == pais_filtro) \
+            .group_by(Registro.classificacao)\
+            .all()
+
+        return dict(sorted(distribuicao, key=lambda tup: tup[1]))
+
+#2: Consulta de qual país com mais imigração entre os meses x e y
+
+def consulta_pais_imigracao(mes_inicial, mes_final):
+    with app.app_context():
+        pais = db.session.query(Registro.pais, db.func.sum(Registro.qtd).label('Total'))\
+                    .filter(Registro.mes.between(mes_inicial, mes_final))\
+                    .group_by(Registro.pais)\
+                    .order_by(db.desc('Total'))\
+                    .first()
+
+        return str(pais.pais)
+
+#3: Consulta de qual é o tipo principal de imigrante que recebemos no brasil entre os meses x e y
+
+def consulta_tipo_imigrante(mes_inicial, mes_final):
+    with app.app_context():
+        tipo = db.session.query(Registro.classificacao, db.func.sum(Registro.qtd).label('Total'))\
+                    .filter(Registro.mes.between(mes_inicial, mes_final))\
+                    .group_by(Registro.classificacao)\
+                    .order_by(db.desc('Total'))\
+                    .first()
+
+        return str(tipo.classificacao)
+
+#4: Consulta de qual é o período do ano que recebmos mais imigrantes do tipo X
+
+def consulta_periodo_popular(classificaçao_filtro):
+    classificaçao_filtro = classificaçao_filtro.capitalize()
+    with app.app_context():
+        periodo = db.session.query(Registro.mes, db.func.sum(Registro.qtd).label('Total'))\
+                    .filter(Registro.classificacao == classificaçao_filtro)\
+                    .group_by(Registro.mes)\
+                    .order_by(db.desc('Total'))\
+                    .first()
+
+        return str(periodo.mes)
+
+def uf_nome_extenso(sigla):
+    filtro = UF.query.filter_by(nome=sigla).first()
+    nome_extenso_uf = filtro.nome_extenso
+    return nome_extenso_uf
+
+# 5: Qual o evento do estado X que chama mais atenção para o imigrante de tipo Y?
+
+def consulta_mes_mais_atrativo(uf_filtro, classificacao_filtro):
+    with app.app_context():
+        registros = db.session.query(Registro.mes, db.func.sum(Registro.qtd).label('Total')).filter(Registro.uf == uf_filtro, Registro.Classificacao == classificacao_filtro).all() \
+            .group_by(Registro.mes) \
+            .order_by(db.desc('Total')) \
+            .first()
+        
+    return str(registros.mes)
+
+# 6: Qual o estado que possui mais registros de imigrantes residentes no mes X?
+
+def consulta_estado_mais_residentes(mes_filtro):
+    with app.app_context():
+        estado = db.session.query(Residente.uf, db.func.count().label('Total')) \
+            .filter(Residente.mes == mes_filtro) \
+            .group_by(Residente.uf) \
+            .order_by(db.desc('Total')) \
+            .first()
+        nome_ext = uf_nome_extenso(str(estado.uf))
+        return(nome_ext)
+
+# 7: Qual estado recebe mais imigrantes do país X?
+
+def consulta_estado_com_mais_imigrantes(pais_filtro):
+    pais_filtro = pais_filtro.upper()
+    with app.app_context():
+        # Filtrar os registros pelo país especificado
+        registros = Registro.query.filter_by(pais=pais_filtro).all()
+        
+        # Calcular a soma da coluna "QTD" para cada estado
+        soma_por_estado = {}
+        for registro in registros:
+            estado = registro.uf
+            soma_por_estado[estado] = soma_por_estado.get(estado, 0) + registro.qtd
+        
+        # Encontrar o estado com a maior soma
+        estado_mais_imigrantes = max(soma_por_estado, key=soma_por_estado.get)
+        
+        # Obter o nome completo do estado
+        nome_ext = uf_nome_extenso(estado_mais_imigrantes)
+        # Retornar o estado e a quantidade de imigrantes
+        return estado_mais_imigrantes, soma_por_estado[estado_mais_imigrantes]
+
+# 8: Qual o tipo de imigração que mais ocorre a partir do País X?
+
+def consulta_imigracao_recorrente_do_pais(pais_filtro):
+    pais_filtro = pais_filtro.upper()
+    with app.app_context():
+        registros = Registro.query.filter_by(pais=pais_filtro).all()
+        soma_por_tipo = {}
+        for registro in registros:
+            tipo_imigraccao = registro.classificacao
+            soma_por_tipo[tipo_imigraccao] = soma_por_tipo.get(tipo_imigraccao, 0) + 1
+        
+        tipo_mais_recorrente = max(soma_por_tipo, key=soma_por_tipo.get)
+        return str(tipo_mais_recorrente)
+
+# 9: Qual a quantidade de imigrante do país no período de maior chegada de imigrantes no país?
+
+def consulta_quantidade_pais_periodo_popular(periodo_popular,pais_filtro):
+    with app.app_context():
+        registros = db.session.query(Registro.pais, db.func.sum(Registro.qtd).label('Total'))\
+            .filter(Registro.pais == pais_filtro, Registro.mes == periodo_popular).all() \
+            .group_by(Registro.pais) \
+            .order_by(db.desc('Total')) \
+            .first()
+        
+    return str(registros.Total)
+
+# 10: Qual a classificação do país X que mais recebemos no tempo Y?
+
+def consulta_classificacao_pais_tempo(pais_filtro, mes_filtro):
+    pais_filtro = pais_filtro.upper()
+    with app.app_context():
+        registros = Registro.query(Registro.classificacao, db.func.sum(Registro.qtd).label('Total')).filter(Registro.pais == pais_filtro, Registro.mes == mes_filtro).all() \
+            .group_by(Registro.classificacao) \
+            .order_by(db.desc('Total')) \
+            .first()
+
+    return str(registros.classificacao)
+
+#Rota 1
+
+@app.route('/api/distribuicao-de-imigrantes-pelo-pais', methods=['POST'])
+def distribuicao_imigrantes_pais():
+    pais_filtro = request.json.get('pais')
+    distribuicao = consulta_distribuicao_imigrantes_pais(pais_filtro)
+    print(distribuicao)
+    return jsonify({'Total_Fronteirico': distribuicao.get('Fronteiriço', 0), 'Total_Provisorio': distribuicao.get('Provisório', 0), 'Total_Residente': distribuicao.get('Residente', 0), 'Total_Temporario': distribuicao.get('Temporário', 0)})
+
+#Rota 2
+@app.route('/api/pais-com-mais-imigracao-no-periodo', methods=['POST'])
+def pais_com_mais_imigracao():
+    meses_filtro = [request.json.get('mes_inicial'), request.json.get('mes_final')]
+    pais = consulta_pais_imigracao(mes_inicial=meses_filtro[0], mes_final=meses_filtro[1])
+    return jsonify({'pais': pais, 'mes_inicial': meses_filtro[0], 'mes_final': meses_filtro[1]})
+
+#Rota 3
+@app.route('/api/tipo-de-imigracao-mais-popular-no-periodo', methods=['POST'])
+def tipo_imigracao_popular():
+    meses_filtro = [request.json.get('mes_inicial'), request.json.get('mes_final')]
+    tipo = consulta_tipo_imigrante(mes_inicial=meses_filtro[0], mes_final=meses_filtro[1])
+    return jsonify({'tipo': tipo, 'mes_inicial': meses_filtro[0], 'mes_final': meses_filtro[1], 'pais': 'Brasil'})
+
+#Rota 4
+@app.route('/api/periodo-mais-popular-para-o-tipo', methods=['POST'])
+def periodo_popular():
+    classificacao_filtro = request.json.get('classificacao')
+    periodo = consulta_periodo_popular(classificaçao_filtro=classificacao_filtro)
+    return jsonify({'periodo': periodo, 'classificacao': classificacao_filtro})
+
+#Rota 5
+
+@app.route('/api/mes-que-chama-mais-atencao-para-o-imigrante-em-um-estado', methods=['POST'])
+def mes_mais_atrativo():
+    uf_filtro = request.json.get('uf')
+    classificacao_filtro = request.json.get('classificacao')
+    registro = consulta_mes_mais_atrativo(uf_filtro=uf_filtro, classificacao_filtro=classificacao_filtro)
+        
+    return jsonify({'mes': registro, 'uf': uf_filtro, 'classificacao': classificacao_filtro})
+
+#Rota 6
+@app.route('/api/estado-com-mais-residentes-no-mes', methods=['POST'])
+def estado_mais_residentes():
+    mes_filtro = request.json.get('mes')
+    estado_mais_residentes = consulta_estado_mais_residentes(mes_filtro=mes_filtro)
+    estado_nome = estados[estado_mais_residentes]
+    return jsonify({'estado': estado_nome, 'mes': mes_filtro})
+
+#Rota 7
+
+@app.route('/api/estado-com-mais-imigrantes', methods=['POST'])
+def estado_mais_imigrantes():
+    pais_filtro = request.json.get('pais')
+    estado_mais_imigrantes, qtd_estado = consulta_estado_com_mais_imigrantes(pais_filtro)
+    estado_nome = estados[estado_mais_imigrantes]
+    return jsonify({'estado': estado_nome, 'quantidade': qtd_estado, 'pais': pais_filtro})
+
+#Rota 8
+
+@app.route('/api/maior-tipo-imigrante-do-pais', methods=['POST'])
+def tipo_imigrante_pais():
+    pais_filtro = request.json.get('pais')
+    tipo_imigrante = consulta_imigracao_recorrente_do_pais(pais_filtro)
+    return jsonify({'pais': pais_filtro, 'tipo': tipo_imigrante})
+
+#Rota 9
+
+@app.route('/api/quantidade-do-pais-no-periodo-de-maior-imigracao', methods=['POST'])
+def pais_imigracao_periodo_popular():
+    pais_filtro = request.json.get('pais')
+    mes_filtro = request.json.get('mes')
+    qtd_pais = consulta_quantidade_pais_periodo_popular(pais_filtro, mes_filtro)
+    return jsonify({'pais': pais_filtro, 'mes': mes_filtro, 'quantidade': qtd_pais})    
 
 
+#Rota 10
 
+@app.route('/api/classificacao-de-imigrante-mais-popular-em-um-mes', methods=['POST'])
+def classificacao_pais_tempo():
+    pais_filtro = request.json.get('pais')
+    mes_filtro = request.json.get('mes')
+    classificacao_popular = consulta_classificacao_pais_tempo(pais_filtro, mes_filtro)
+    return jsonify({'pais': pais_filtro, 'mes': mes_filtro, 'classificacao': classificacao_popular})
 
 # ------------- ROTAS DA API -------------
-# Executa o cadastro linha a linha dos arquivos csv. Aciona a execução dos cadastros secundários
-@app.route('/registros', methods=['POST'])
-def cadastrar_registro():
-    # Verifica se a solicitação possui todos os campos necessários
-    if not request.json or 'uf' not in request.json or 'pais' not in request.json or 'classificacao' not in request.json or 'qtd' not in request.json:
-        return jsonify({'error': 'Solicitação inválida. Certifique-se de fornecer todos os campos necessários.'}), 400
-
-    # Cria um novo registro
-    new_registro = Registro(
-        request.json['uf'],
-        request.json['pais'],
-        request.json['classificacao'],
-        request.json['qtd'],
-        request.json['mes']
-    )
-
-    # Converte a classificação para minúsculas
-    classificacao = new_registro.classificacao.lower()
-
-    # Cadastra o país e o estado
-    cadastrar_pais(new_registro.pais)
-    cadastrar_uf(new_registro.uf)
-
-    # Executa a ação apropriada com base na classificação
-    if classificacao == 'residente':
-        cadastrar_residente(new_registro.uf, new_registro.pais, new_registro.qtd)
-    elif classificacao == 'provisório':
-        cadastrar_provisorio(new_registro.uf, new_registro.pais, new_registro.qtd)
-    elif classificacao == 'temporario':
-        cadastrar_temporario(new_registro.uf, new_registro.pais, new_registro.qtd)
-    else:
-        cadastrar_fronteirico(new_registro.uf, new_registro.pais, new_registro.qtd)
-
-    # Adiciona o novo registro ao banco de dados
-    db.session.add(new_registro)
-    db.session.commit()
-
-    # Retorna os dados do registro inserido e o código de status 200
-    return jsonify({
-        'id': new_registro.id,
-        'uf': new_registro.uf,
-        'pais': new_registro.pais,
-        'classificacao': new_registro.classificacao,
-        'qtd': new_registro.qtd
-    }), 200
-
-# ------------- ROTAS DA API -------------
-
-# with app.app_context():
-#     db.create_all()
--- src/control.py
+++ src/control.py
@@ -7,6 +7,41 @@ from models import *
 def index():
     return render_template('index.html')
 
+# q1
+@app.route('/distribuicao_imigrantes_pais', methods=['POST'])
+def func_distribuicao_imigrantes_pais():
+    pais_filtro = request.form['pais_filtro_distribuicao_imigrantes_pais']
+    data = {'pais': pais_filtro}
+    response = requests.post('http://localhost:5000/api/distribuicao-de-imigrantes-pelo-pais', json=data)
+    if response.status_code == 200:
+        result = response.json()
+        total_front = str(result['Total_Fronteirico'])
+        total_prov = str(result['Total_Provisorio'])
+        total_res = str(result['Total_Residente'])
+        total_temp = str(result['Total_Temporario'])
+        resultado = 'Total de imigrantes Fronteiriços: ' + total_front + '<br>'
+        resultado += 'Total de imigrantes Provisórios: ' + total_prov + '<br><br>'
+        resultado += 'Total de imigrantes Residentes: ' + total_res + '<br><br>'
+        resultado += 'Total de imigrantes Temporários: ' + total_temp + '<br>'
+        consulta = 'Distribuição de imigrantes: ' + pais_filtro
+        return render_template('result.html', resultado=resultado, consulta_nome=consulta)
+
+
+# #q2
+# @app.route('/', methods=[''])
+# # q3
+# @app.route('/', methods=[''])
+# # q4
+# @app.route('/', methods=[''])
+# # q5
+# @app.route('/', methods=[''])
+# # q6
+# @app.route('/', methods=[''])
+
+
+
+
+# q7
 @app.route('/estado_mais_imigrantes', methods=['POST'])
 def func_estado_mais_imigrantes():
     pais_filtro = request.form['pais_filtro_estado_mais_imigrantes']
@@ -15,12 +50,19 @@ def func_estado_mais_imigrantes():
     if response.status_code == 200:
         result = response.json()
         estado_nome = result['estado']
-        qtd_estado = result['quantidade']
-        return render_template('index.html', retorno_estado_mais_imigrantes=estado_nome, qtd_estado_mais_imigrantes=qtd_estado)
+        qtd_estado = str(result['quantidade'])
+        resultado = 'Estado: '+ estado_nome + ' QTD: ' + qtd_estado
+
+        return render_template('result.html',resultado=resultado, consulta_nome='Estado com mais imigrantes de determinado País.')
     else:
         return 'Erro ao obter o estado com mais imigrantes'
     
-
+# # q8
+# @app.route('/', methods=[''])
+# # q9
+# @app.route('/', methods=[''])
+# # q10
+# @app.route('/', methods=[''])
 
 if __name__ == '__main__':
     app.run()
